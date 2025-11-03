55a8b9598a3862d87080634327c66be48f8925d8
DeltaByte <filbert.willy@gmail.com>
2025-11-03 11:01:05 +0700

    Somewhat cleaned up code and added temporary about us page

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
+@app.route("/Home")
 def home():
    return render_template("index.html")
 
-@app.route("/events")
+@app.route("/Events")
 def events():
    return render_template("events.html")
+
+@app.route("/About_Us")
+def about_us():
+   return render_template("about_us.html")
 
 if __name__ == "__main__":
     app.run()
