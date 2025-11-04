2588480dbe3ff83161db552c736d8092f3fd07c2
DeltaByte <filbert.willy@gmail.com>
2025-11-04 20:12:15 +0700

    Added placeholder Meloria Event page and added :root to css (continue later)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# Main Pages
 @app.route("/")
 @app.route("/Home")
 def home():
@@ -13,6 +14,11 @@ def events():
 @app.route("/About_Us")
 def about_us():
    return render_template("about_us.html")
+
+# Event Pages
+@app.route("/Events/Meloria")
+def meloria():
+   return render_template("events/meloria.html")
 
 if __name__ == "__main__":
     app.run()
