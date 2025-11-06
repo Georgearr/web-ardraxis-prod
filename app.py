efadfe7ca4c8ba93e35e36ec58ae43e03fc4cac6
George A <Georgearrev@gmail.com>
2025-11-06 12:29:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
    return render_template("index.html")
 
+@app.route("/coming_soon")
+def coming_soon():
+    return render_template("coming_soon.html")
+
 if __name__ == "__main__":
     app.run()
